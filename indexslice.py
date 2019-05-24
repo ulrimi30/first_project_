6fcb34f9135e21a955b54de2aaa918ccc59103b4
ulrimi30 <ulrimi30@gmail.com>
2019-05-24 15:40:27 -0400

    Add files via upload
    test file

---
+++ indexslice.py
@@ -0,0 +1,6 @@
+phoneNumber = "1-555-825-8347"
+
+print("country code: " , phoneNumber[0:1:1])
+print("area code: " , phoneNumber[2:5:1])
+print("prefix:", phoneNumber[6:9:1])
+print("line number: " , phoneNumber[10: :1])
